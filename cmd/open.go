82f91de70dfb69156a36fb300aee1ec24d470834
Nathan Evans <nathan.evans@datadoghq.com>
2026-02-20 11:12:23 -0700

    Make gopen without a pr look at the github file tree

--- cmd/open.go
+++ cmd/open.go
@@ -20,32 +20,32 @@ if command -v gh &> /dev/null; then
   fi
 fi
 
-# No existing PR found, fall back to compare URL
-# Retrieve destination branch
-destination_branch=${branch%--*}
-
-# Determine the default origin branch
-if [[ $branch == $destination_branch ]]; then
-  master_exists=$(git branch | grep main)
-
-  if [[ -z $master_exists ]]; then
-	destination_branch=develop
-  else
-	destination_branch=main
-  fi
-fi
-
+# No existing PR found, open file explorer at current directory
 # Retrieve repository URL
 repository_url=$(git remote get-url origin | sed -e 's/git@//' -e 's/\.git//' -e 's/:/\//')
 
+# Get the path relative to the git root
+git_root=$(git rev-parse --show-toplevel)
+current_dir=$(pwd)
+relative_path=${current_dir#$git_root}
+
+# Build the file explorer URL
 if [[ $repository_url == github* ]]; then
-  pr_url=https://$repository_url/compare/$destination_branch...$branch
+  if [[ -z $relative_path || $relative_path == "/" ]]; then
+    file_url="https://$repository_url/tree/$branch"
+  else
+    file_url="https://$repository_url/tree/$branch$relative_path"
+  fi
 elif [[ $repository_url == gitlab* ]]; then
-  pr_url="https://$repository_url/merge_requests/new?merge_request%5Bsource_branch%5D=$branch&merge_request%5Btarget_branch%5D=$destination_branch"
+  if [[ -z $relative_path || $relative_path == "/" ]]; then
+    file_url="https://$repository_url/-/tree/$branch"
+  else
+    file_url="https://$repository_url/-/tree/$branch$relative_path"
+  fi
 fi
 
-# Open the new Pull Request URL
-open $pr_url
+# Open the file explorer URL
+open $file_url
 `
 
 var OpenCmd = &cobra.Command{
@@ -53,6 +53,6 @@ var OpenCmd = &cobra.Command{
 	Args: cobra.NoArgs,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		shell.BashStatusCode(script)
-		return audit.Write(branch, "opening pull request")
+		return audit.Write(branch, "opening in browser")
 	},
 }
